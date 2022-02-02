2c7ddb36fd5c03cf2f27d1fbc4d96829cd4192cc
nobijoy <nobijoy1446@gmail.com>
2022-02-02 13:03:52 +0600

    string

--- app/classes/Series.php
+++ app/classes/Series.php
@@ -1,21 +1,22 @@
 <?php
 namespace App\classes;
-
-
 class Series
 {
+
     protected $startingNumber;
     protected $endingNumber;
     protected $result;
     protected $i;
     protected $oddEven;
-    protected $user;
 
     public function __construct($post=null)
     {
         $this->startingNumber = $post['starting_number'];
         $this->endingNumber = $post['ending_number'];
-        $this->oddEven = $post['odd_even'];
+
+        if(isset($post['odd_even'])){
+            $this->oddEven = $post['odd_even'];
+        }
 
     }
 
@@ -23,28 +24,62 @@ class Series
         header('Location: pages/index.php');
     }
 
-    public function makeSeries(){
-        if ($this->startingNumber > $this->endingNumber){
-            return $this->bigToSmallSeries();
+    public function makeSeries()
+    {
+        if($this->startingNumber>$this->endingNumber)
+        {
+            return $this->btosSeries();
         }
-        else{
-            return $this->smallToBigSeries();
+        else
+        {
+            if ($this->oddEven == 'odd')
+            {
+                return $this->stobOddSeries();
+            }
+            else if($this->oddEven == 'even')
+            {
+                return $this->stobEvenSeries();
+            }
+            else{
+                return $this->stobSeries();
+            }
+
         }
     }
 
-    protected function smallToBigSeries(){
-        for ($this->i= $this->startingNumber; $this->i <=$this->endingNumber; $this->i++){
-            $this->result .= $this->i.' ';
-        }
+    protected function stobSeries(){
+        for ($this->i = $this->startingNumber; $this->i <= $this->endingNumber; $this->i++)
+        {
 
+            $this->result.= $this->i.' ';
+        }
+        return $this->result;
+    }
+    protected function btosSeries(){
+        for ($this->i = $this->startingNumber; $this->i >= $this->endingNumber; $this->i--)
+        {
+            $this->result.= $this->i.' ';
+        }
         return $this->result;
     }
 
-    protected function bigToSmallSeries(){
-        for ($this->i= $this->startingNumber; $this->i >= $this->endingNumber; $this->i--){
-            $this->result .= $this->i.' ';
+    protected function stobOddSeries(){
+        for ($this->i = $this->startingNumber; $this->i <= $this->endingNumber; $this->i++)
+        {
+            if($this->i % 2 != 0){
+                $this->result.= $this->i.' ';
+            }
+
         }
         return $this->result;
     }
-
+    protected function stobEvenSeries(){
+        for ($this->i = $this->startingNumber; $this->i <= $this->endingNumber; $this->i++)
+        {
+            if($this->i % 2 == 0){
+                $this->result.= $this->i.' ';
+            }
+        }
+        return $this->result;
+    }
 }
